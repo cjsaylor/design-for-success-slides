ec61fbab412fd287a0e2ea7c52ffc4174929f38f
Chris Saylor <cjsaylor@gmail.com>
2017-11-30 12:06:13 -0500

    Add example of using knobs addon.

--- example/stories.js
+++ example/stories.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { storiesOf, action } from '@storybook/react';
 import List from './list-component';
 
+import {withKnobs, array} from '@storybook/addon-knobs';
+
 storiesOf('List', module)
+	.addDecorator(withKnobs)
 	.add('No items', () => <List />)
 	.add('One item', () => <List items={['item 1']} />)
 	.add('Multiple items', () => {
-		return <List onItemClicked={action('Item clicked!')} items={['item 1', 'item 2', 'item 3']} />;
+		return <List onItemClicked={action('Item clicked!')} items={array('Items', ['item 1', 'item 2', 'item 3'])} />;
 	});
